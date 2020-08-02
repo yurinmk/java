123bc6155ca85bf93253d9f2de9acfd9c40fed5e
Yuri Christian <yuri-tech@outlook.com>
2020-08-02 12:11:11 -0300

    Aprendendo a ler arquivo

---
+++ Curso Udemy/LerArquivo/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		File file = new File("G:\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch(IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}finally {
+			if(sc != null) {
+				sc.close();
+			}
+			
+		}
+
+	}
+
+}
