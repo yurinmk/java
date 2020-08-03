86818bf7cf2cd5d5349e747171d7f001a5711d09
Yuri Christian <yuri-tech@outlook.com>
2020-08-03 18:04:49 -0300

    Problema resolvido sem interface

---
+++ Curso Udemy/LocadoraCarro/src/model/services/RentalServices.java
@@ -0,0 +1,32 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalServices {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxServices taxService;
+
+	public RentalServices(Double pricePerDay, Double pricePerHour, BrazilTaxServices taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long duration = carRental.getFinish().getTime() - carRental.getStart().getTime();
+		double hours = (double) duration / 1000 / 60 / 60;
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * this.pricePerHour;
+		}else {
+			basicPayment = Math.ceil(hours / 24) * this.pricePerDay;
+		}
+		double tax = this.taxService.tax(basicPayment);
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+	
+}
